34b43c9338e552fc849f12dfb3a7ad4cdaab777d
SAYANTA-ADHIKARI <sayanta88749@gmail.com>
2023-02-13 22:58:30 +0530

    Initial Commit

---
+++ Assignment_1/mlp.py
@@ -0,0 +1,216 @@
+import numpy as np
+from random import random
+import matplotlib.pyplot as plt
+
+
+class MLP(object):
+    """A Multilayer Perceptron class.
+    """
+
+    def __init__(self, num_inputs=3, hidden_layers=[3, 3], num_outputs=2):
+        """Constructor for the MLP. Takes the number of inputs,
+            a variable number of hidden layers, and number of outputs
+        Args:
+            num_inputs (int): Number of inputs
+            hidden_layers (list): A list of ints for the hidden layers
+            num_outputs (int): Number of outputs
+        """
+
+        self.num_inputs = num_inputs
+        self.hidden_layers = hidden_layers
+        self.num_outputs = num_outputs
+
+        # create a generic representation of the layers
+        layers = [num_inputs] + hidden_layers + [num_outputs]
+
+        # create random connection weights for the layers
+        weights = []
+        for i in range(len(layers) - 1):
+            w = np.random.rand(layers[i], layers[i + 1])
+            weights.append(w)
+        self.weights = weights
+
+        # save derivatives per layer
+        derivatives = []
+        for i in range(len(layers) - 1):
+            d = np.zeros((layers[i], layers[i + 1]))
+            derivatives.append(d)
+        self.derivatives = derivatives
+
+        # save activations per layer
+        activations = []
+        for i in range(len(layers)):
+            a = np.zeros(layers[i])
+            activations.append(a)
+        self.activations = activations
+
+
+    def forward_propagate(self, inputs):
+        """Computes forward propagation of the network based on input signals.
+        Args:
+            inputs (ndarray): Input signals
+        Returns:
+            activations (ndarray): Output values
+        """
+
+        # the input layer activation is just the input itself
+        activations = inputs
+
+        # save the activations for backpropogation
+        self.activations[0] = activations
+
+        # iterate through the network layers
+        for i, w in enumerate(self.weights):
+            # calculate matrix multiplication between previous activation and weight matrix
+            net_inputs = np.dot(activations, w)
+
+            # apply sigmoid activation function
+            activations = self._sigmoid(net_inputs)
+
+            # save the activations for backpropogation
+            self.activations[i + 1] = activations
+
+        # return output layer activation
+        return activations
+
+
+    def back_propagate(self, error):
+        """Backpropogates an error signal.
+        Args:
+            error (ndarray): The error to backprop.
+        Returns:
+            error (ndarray): The final error of the input
+        """
+
+        # iterate backwards through the network layers
+        for i in reversed(range(len(self.derivatives))):
+
+            # get activation for previous layer
+            activations = self.activations[i+1]
+
+            # apply sigmoid derivative function
+            delta = error * self._sigmoid_derivative(activations)
+
+            # reshape delta as to have it as a 2d array
+            delta_re = delta.reshape(delta.shape[0], -1).T
+
+            # get activations for current layer
+            current_activations = self.activations[i]
+
+            # reshape activations as to have them as a 2d column matrix
+            current_activations = current_activations.reshape(current_activations.shape[0],-1)
+
+            # save derivative after applying matrix multiplication
+            self.derivatives[i] = np.dot(current_activations, delta_re)
+
+            # backpropogate the next error
+            error = np.dot(delta, self.weights[i].T)
+
+
+    def train(self, inputs, targets, epochs, learning_rate):
+        """Trains model running forward prop and backprop
+        Args:
+            inputs (ndarray): X
+            targets (ndarray): Y
+            epochs (int): Num. epochs we want to train the network for
+            learning_rate (float): Step to apply to gradient descent
+        """
+        # now enter the training loop
+        for i in range(epochs):
+            sum_errors = 0
+
+            # iterate through all the training data
+            for j, input in enumerate(inputs):
+                target = targets[j]
+
+                # activate the network!
+                output = self.forward_propagate(input)
+
+                error = target - output
+
+                self.back_propagate(error)
+
+                # now perform gradient descent on the derivatives
+                # (this will update the weights
+                self.gradient_descent(learning_rate)
+
+                # keep track of the MSE for reporting later
+                sum_errors += self._mse(target, output)
+
+            # Epoch complete, report the training error
+            print("Error: {} at epoch {}".format(sum_errors / len(items), i+1))
+
+        print("Training complete!")
+        print("=====")
+
+
+    def gradient_descent(self, learningRate=1):
+        """Learns by descending the gradient
+        Args:
+            learningRate (float): How fast to learn.
+        """
+        # update the weights by stepping down the gradient
+        for i in range(len(self.weights)):
+            weights = self.weights[i]
+            derivatives = self.derivatives[i]
+            weights += derivatives * learningRate
+
+
+    def _sigmoid(self, x):
+        """Sigmoid activation function
+        Args:
+            x (float): Value to be processed
+        Returns:
+            y (float): Output
+        """
+
+        y = 1.0 / (1 + np.exp(-x))
+        return y
+
+
+    def _sigmoid_derivative(self, x):
+        """Sigmoid derivative function
+        Args:
+            x (float): Value to be processed
+        Returns:
+            y (float): Output
+        """
+        return x * (1.0 - x)
+
+
+    def _mse(self, target, output):
+        """Mean Squared Error loss function
+        Args:
+            target (ndarray): The ground trut
+            output (ndarray): The predicted values
+        Returns:
+            (float): Output
+        """
+        return np.average((target - output) ** 2)
+
+
+if __name__ == "__main__":
+
+    def concentric_circles(n_classes: int = 2, radii: list = [12, 20], n_samples = 500):
+        noise = np.random.randn(n_samples)
+        x1 = (radii[0] + noise) * np.cos(np.linspace(0, 2*np.pi, n_samples))
+        x2 = ((radii[0] + noise) * np.sin(np.linspace(0, 2*np.pi, n_samples)))
+        x_in = np.vstack([x1/20, x2/20])
+        plt.scatter(x1/20, x2/20)
+        noise = np.random.randn(n_samples)
+        x1 = (radii[1] + noise) * np.cos(np.linspace(0, 2*np.pi, n_samples))
+        x2 = (radii[1] + noise) * np.sin(np.linspace(0, 2*np.pi, n_samples))
+        x_out = np.vstack([x1/20, x2/20])
+        plt.scatter(x1/20, x2/20)
+        X = np.ones([2, 2 * n_samples])
+        Y = np.zeros([1, 2 * n_samples])
+        X[:2, ::2] = x_in
+        X[:2, 1::2] = x_out
+        Y[:, ::2] = 1
+        Y[:, 1::2] = 0
+        plt.xlabel("feature1")
+        plt.ylabel("feature2")
+        plt.title("Dataset")
+        return X.reshape(-1, 2), Y.reshape(-1, 1)
+
+    X, Y = concentric_circles()
